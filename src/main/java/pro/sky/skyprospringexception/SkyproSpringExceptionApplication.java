ea91105d69b1e37aed4149b7303c0333190b9370
ved7997 <ved7997@gmail.com>
2022-03-03 18:16:18 +0300

    первый комит

--- src/main/java/pro/sky/skyprospringexception/SkyproSpringExceptionApplication.java
+++ src/main/java/pro/sky/skyprospringexception/SkyproSpringExceptionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkyproSpringExceptionApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(SkyproSpringExceptionApplication.class, args);
 	}
