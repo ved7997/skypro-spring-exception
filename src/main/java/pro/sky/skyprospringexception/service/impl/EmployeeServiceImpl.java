858fae36a351c0982439a061f8b77ca2452ced92
ved7997 <ved7997@gmail.com>
2022-03-28 20:07:02 +0300

    Третий комит первая ветка 28.03.2022

--- src/main/java/pro/sky/skyprospringexception/service/impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/skyprospringexception/service/impl/EmployeeServiceImpl.java
@@ -49,6 +49,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public  Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
+
         if (!employees.contains(employee)) {
             throw new EmployeeNotFoundException();
         }
